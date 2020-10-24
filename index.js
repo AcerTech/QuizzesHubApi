fba194ee7b0ab0fbe101af4c6cc359528b33531f
AcerTech <acerTech20@gmail.com>
2020-10-23 21:30:53 -0500

    some fixing1

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ var cors = require('cors')
 
 app.use(cors())
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-})
-
 const dotenv = require('dotenv');
 dotenv.config();
 
